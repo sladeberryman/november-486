3bf4bf2930c71e0a1d81bcdef5c574b444c5aa89
sladeberryman <slade12berryman@gmail.com>
2024-01-30 18:51:46 -0600

    yo yo from ejs

--- app.js
+++ app.js
@@ -2,10 +2,28 @@ const express = require('express')
 const app = express()
 const port = process.env.PORT || 5500;
 
-app.get('/', function (req, res) {
-    res.sendFile(__dirname + '/index.html');
+let path = require('path');
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+
+
+let myTypeServer = "Reformer";
+
+app.get('/', function(req, res) {
+
+  res.render('index', {
+   
+    myTypeClient: myTypeServer 
+
+  });
+  
 });
 
+app.get('/home', function (req, res) {
+  res.sendFile(__dirname + '/index.html');
+});
+
+
 app.listen(port, () => {
-  console.log(`MIKE app listening on port ${port}`)
+  console.log(`nov app listening on port ${port}`)
 })
